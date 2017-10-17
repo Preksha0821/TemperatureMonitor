a76f13341d623408256420157cab4bf8e0a55ef2
preksha0821 <preksha.21893@gmail.com>
2017-10-17 09:52:54 -0500

    Seperated code in 3 different components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {TemperatureMonitorService} from './app.temperatureMonitor.service';
 
 @Component({
   selector: 'app-root',
@@ -7,39 +6,4 @@ import {TemperatureMonitorService} from './app.temperatureMonitor.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  temperature: number;
-  count = 0;
-  exceedLimit = false;
-  exceedLimitText = '';
-  currentMedian = null;
-  getMedian = false;
-  constructor(public temperatureMonitorService: TemperatureMonitorService) {
-  }
-
-  tempListLength() {
-    return this.temperatureMonitorService.temperature_list.length;
-  }
-
-  tempList(){
-    return this.temperatureMonitorService.temperature_list;
-  }
-
-  getCurrentMedian() {
-    let sortedTempList = this.temperatureMonitorService.temperature_list.sort((a, b) => a - b);
-
-    this.currentMedian = (sortedTempList[(sortedTempList.length - 1) >> 1] + sortedTempList[sortedTempList.length >> 1]) / 2;
-    this.getMedian=true;
-    console.log(sortedTempList, this.currentMedian);
-  }
-
-  recordTemperature(temp: number): void {
-    this.count++;
-    if (this.count < 9) {
-      this.temperatureMonitorService.recordTemperature(temp);
-      //this.temperature = null;
-    } else {
-      this.exceedLimitText = 'you have exceed your limits';
-      this.exceedLimit = true;
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {TemperatureMonitorService} from './app.temperatureMonitor.service';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AddTemperatureComponent } from './add-temperature/add-temperature.component';
+import { GetMedianComponent } from './get-median/get-median.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AddTemperatureComponent,
+    GetMedianComponent,
+    ProgressBarComponent,
   ],
   imports: [
     BrowserModule,
